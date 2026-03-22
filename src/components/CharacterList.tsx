8ee4dadf807ea538bc9a321bb877cb451b393585
Danilo Queiroz <danilof.queiroz1@gmail.com>
2026-03-21 23:52:50 -0300

    feat: implement grid and list view toggle

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -1,19 +1,31 @@
 'use client'
+import { useState } from "react";
 import type { Character } from "@/types/character";
 import CharacterCard from "./CharacterCard";
+import ToggleList from "./ToggleList";
 
 interface CharacterListProps {
   characters: Character[];
 }
 
 export default function CharacterList({characters}: CharacterListProps) {
+  const [isGridView, setisGridView] = useState(true)
   return (
-    <div className="flex-1 flex flex-col gap-5">
-      {characters.map((char) => (
-        <div key={char.id} className="max-w-52">
-          <CharacterCard character={char} />
-        </div>
-      ))}
-    </div>
+    <section className="flex flex-col items-center px-5 w-full">
+      <ToggleList isGridView={isGridView} onClick={setisGridView} />
+      <div
+        className={
+          isGridView
+            ? "grid grid-cols-4 gap-3"
+            : "flex flex-col gap-y-5 w-80 px-5"
+        }
+      >
+        {characters.map((char) => (
+          <div key={char.id} className={isGridView ?"max-w-52" : ""}>
+            <CharacterCard isGrid={isGridView} character={char} />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
