38637909eed82be388ad0151dc1baf83c7a5bad2
Danilo Queiroz <danilof.queiroz1@gmail.com>
2026-03-22 11:10:13 -0300

    fix: removed unused import

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Image from "next/image";
 import Link from "next/link";
 import { use, useEffect, useState } from "react";
 import CharacterDetailsCard from "@/components/CharacterDetailsCard";
